00cacfee02f7540f6f69f0874df26f5bf1189fa3
Lauriel Mesquita <laurielmesquita@me.com>
2019-06-01 11:16:11 -0300

    Business description added

--- components/Head.js
+++ components/Head.js
@@ -11,35 +11,35 @@ function HeadPage() {
                 content='width=device-width, initial-scale=1, shrink-to-fit=no'
             />
             <meta name='keywords' content='true' />
-            <title>Gestão & Mais</title>
-            <meta name='description' content='Agência de negócios' />
+            <title>Gestão & Mais - Agência de Negócios</title>
+            <meta name='description' content='Com mais de 10 anos no mercado oferecemos um leque de soluções para gestão empresarial, assessorando empresas a se desenvolverem no seu mercado.' />
             <link
                 rel='canonical'
-                href='https://coding.laurielmesquita.now.sh/'
+                href='https://gestaoemais.com.br/'
             />
             <meta name='robots' content='index, follow' />
             {/* <!-- Twitter --> */}
             <meta name='twitter:card' content='summary_large_image' />
             <meta
                 name='twitter:url'
-                content='https://coding.laurielmesquita.now.sh/'
+                content='https://gestaoemais.com.br/'
             />
-            <meta name='twitter:title' content='Gestão & Mais' />
-            <meta name='twitter:description' content='Agência de negócios' />
+            <meta name='twitter:title' content='Gestão & Mais - Agência de Negócios' />
+            <meta name='twitter:description' content='Com mais de 10 anos no mercado oferecemos um leque de soluções para gestão empresarial, assessorando empresas a se desenvolverem no seu mercado.' />
             <meta
                 name='twitter:image:src'
-                content='https://coding.laurielmesquita.now.sh/static/social_image.jpg'
+                content='https://gestaoemais.com.br/static/social_image.jpg'
             />
             {/* <!-- Open Graph general --> */}
             <meta
                 property='og:url'
-                content='https://coding.laurielmesquita.now.sh/'
+                content='https://gestaoemais.com.br/'
             />
-            <meta property='og:title' content='Gestão & Mais' />
-            <meta property='og:description' content='Agência de negócios' />
+            <meta property='og:title' content='Gestão & Mais - Agência de Negócios' />
+            <meta property='og:description' content='Com mais de 10 anos no mercado oferecemos um leque de soluções para gestão empresarial, assessorando empresas a se desenvolverem no seu mercado.' />
             <meta
                 property='og:image'
-                content='https://coding.laurielmesquita.now.sh/static/social_image.jpg'
+                content='https://gestaoemais.com.br/static/social_image.jpg'
             />
             <meta property='og:image:type' content='image/jpeg' />
             <meta property='og:image:width' content='1200' />
@@ -50,7 +50,7 @@ function HeadPage() {
             <link
                 rel='icon'
                 type='image/png'
-                href='https://coding.laurielmesquita.now.sh/static/favicon.png'
+                href='https://gestaoemais.com.br/static/favicon.png'
             />
             {/* <!-- Scripts --> */}
             <script src='https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15/dist/smooth-scroll.polyfills.min.js' />
